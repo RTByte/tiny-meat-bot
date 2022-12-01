adcc8649295aa357fdc905e5e864a424f61bdff8
Rasmus Gerdin <rasmus@rasmusgerdin.com>
2022-12-01 22:54:57 +0100

    feat(listeners): emoji tracking

---
+++ src/listeners/guilds/emojis/emojiDelete.ts
@@ -0,0 +1,17 @@
+import { ApplyOptions } from "@sapphire/decorators";
+import { Events, Listener, ListenerOptions } from "@sapphire/framework";
+import { GuildEmoji } from "discord.js";
+
+@ApplyOptions<ListenerOptions>({ event: Events.GuildEmojiDelete })
+export class UserListener extends Listener {
+	public async run(emoji: GuildEmoji) {
+		await this.container.client.prisma.guild.update({
+			where: { id: String(emoji.guild.id) },
+			data: {
+				emojisDeleted: {
+					increment: 1
+				}
+			}
+		})
+	}
+}
