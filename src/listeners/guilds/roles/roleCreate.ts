53a5cebd26e3cad53d3f2826364acb90d0e66ccd
Rasmus Gerdin <rasmus@rasmusgerdin.com>
2022-12-02 00:14:00 +0100

    feat(listeners): role tracking

---
+++ src/listeners/guilds/roles/roleCreate.ts
@@ -0,0 +1,17 @@
+import { ApplyOptions } from "@sapphire/decorators";
+import { Events, Listener, ListenerOptions } from "@sapphire/framework";
+import { Role } from "discord.js";
+
+@ApplyOptions<ListenerOptions>({ event: Events.GuildRoleCreate })
+export class UserListener extends Listener {
+	public async run(role: Role) {
+		await this.container.client.prisma.guild.update({
+			where: { id: String(role.guild.id) },
+			data: {
+				rolesCreated: {
+					increment: 1
+				}
+			}
+		});
+	}
+}
---
+++ src/listeners/guilds/roles/roleDelete.ts
@@ -0,0 +1,17 @@
+import { ApplyOptions } from "@sapphire/decorators";
+import { Events, Listener, ListenerOptions } from "@sapphire/framework";
+import { Role } from "discord.js";
+
+@ApplyOptions<ListenerOptions>({ event: Events.GuildRoleDelete })
+export class UserListener extends Listener {
+	public async run(role: Role) {
+		await this.container.client.prisma.guild.update({
+			where: { id: String(role.guild.id) },
+			data: {
+				rolesDeleted: {
+					increment: 1
+				}
+			}
+		});
+	}
+}
