8c211a8d53110b7706023b35d1485e64944898b1
Rasmus Gerdin <rasmus@rasmusgerdin.com>
2022-12-01 22:12:34 +0100

    feat: add example config file

---
+++ src/config.example.ts
@@ -0,0 +1,35 @@
+import { LogLevel } from '@sapphire/framework';
+import { ClientOptions } from 'discord.js';
+
+export const DEV = process.env.NODE_ENV !== 'production';
+
+export const CLIENT_ID = '';
+export const OWNERS: string[] = [''];
+export const PREFIX = '';
+export const VERSION = '0.0.0';
+
+export const CLIENT_OPTIONS: ClientOptions = {
+	caseInsensitiveCommands: true,
+	caseInsensitivePrefixes: true,
+	defaultPrefix: PREFIX,
+	regexPrefix: /^(hey +)?bot[,! ]/i,
+	shards: 'auto',
+	intents: [],
+	loadDefaultErrorListeners: false,
+	partials: [],
+	presence: {
+		activities: [
+			{
+				name: '',
+				type: 3
+			}
+		]
+	},
+	logger: {
+		level: DEV ? LogLevel.Debug : LogLevel.Info
+	},
+};
+
+export const TOKENS = {
+	BOT_TOKEN: '',
+};
