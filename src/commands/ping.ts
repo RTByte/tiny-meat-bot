946441b9738378981a79b7f7dfdaff9a8e03b888
Rasmus Gerdin <rasmus@rasmusgerdin.com>
2022-12-04 17:03:40 +0100

    refactor(cmds): update description for ping cmd

--- src/commands/ping.ts
+++ src/commands/ping.ts
@@ -1,26 +1,30 @@
 import { isMessageInstance } from '@sapphire/discord.js-utilities';
 import { ChatInputCommand, Command } from '@sapphire/framework';
 
-export class PingCommand extends Command {
-  public constructor(context: Command.Context, options: Command.Options) {
-    super(context, { ...options });
-  }
+export class UserCommand extends Command {
+	public constructor(context: Command.Context, options: Command.Options) {
+		super(context, {
+			...options,
+			name: 'ping',
+			description: 'Pings Tiny Meat Bot'
+		});
+	}
 
-  public override registerApplicationCommands(registry: ChatInputCommand.Registry) {
-    registry.registerChatInputCommand((builder) =>
-      builder.setName('ping').setDescription('Runs a connection test')
-    );
-  }
+	public override registerApplicationCommands(registry: ChatInputCommand.Registry) {
+		registry.registerChatInputCommand((builder) =>
+			builder.setName('ping').setDescription('Pings Tiny Meat Bot')
+		);
+	}
 
-  public async chatInputRun(interaction: Command.ChatInputInteraction) {
-    const msg = await interaction.reply({ content: `Ping?`, ephemeral: true, fetchReply: true });
+	public async chatInputRun(interaction: Command.ChatInputInteraction) {
+    	const msg = await interaction.reply({ content: `Ping?`, ephemeral: true, fetchReply: true });
 
-    if (isMessageInstance(msg)) {
-      const diff = msg.createdTimestamp - interaction.createdTimestamp;
-      const ping = Math.round(this.container.client.ws.ping);
-      return interaction.editReply(`✅ Pong! \`Bot: ${diff}ms\` \`API: ${ping}ms\``);
-    }
+		if (isMessageInstance(msg)) {
+			const diff = msg.createdTimestamp - interaction.createdTimestamp;
+			const ping = Math.round(this.container.client.ws.ping);
+			return interaction.editReply(`✅ Pong! \`Bot: ${diff}ms\` \`API: ${ping}ms\``);
+		}
 
-    return interaction.editReply('❌ Failed to retrieve ping.');
-  }
+		return interaction.editReply('❌ Failed to retrieve ping.');
+	}
 }
