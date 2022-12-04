a3d0a76b25a272f626bf047cf32b12d3445b42a3
Rasmus Gerdin <rasmus@rasmusgerdin.com>
2022-12-04 17:02:36 +0100

    feat(db): add last reset entry to client model

--- src/commands/reset.ts
+++ src/commands/reset.ts
@@ -1,13 +1,19 @@
 import { ChatInputCommand, Command } from '@sapphire/framework';
 
-export class PingCommand extends Command {
+export class UserCommand extends Command {
   public constructor(context: Command.Context, options: Command.Options) {
-    super(context, { ...options });
+    super(context, {
+		...options,
+		name: 'reset',
+		description: 'Reset Tiny Meat Bot database'
+	});
   }
 
   public override registerApplicationCommands(registry: ChatInputCommand.Registry) {
-    registry.registerChatInputCommand((builder) =>
-      builder.setName('reset').setDescription('Reset Tiny Meat Bot database')
+    registry.registerChatInputCommand((builder) => builder
+		.setName('reset')
+		.setDescription('Reset Tiny Meat Bot database')
+		.setDefaultMemberPermissions('0')
     );
   }
 
@@ -19,6 +25,13 @@ export class PingCommand extends Command {
 	const deleteMember = await this.container.client.prisma.member.deleteMany({});
 	const deleteEmoji = await this.container.client.prisma.emoji.deleteMany({});
 	const deleteSticker = await this.container.client.prisma.sticker.deleteMany({});
+
+	await this.container.client.prisma.client.update({
+		where: { id: this.container.client.id as string },
+		data: {
+			lastReset: new Date()
+		}
+	})
 
     return interaction.editReply(`✅ Database reset. \n\n**Removed:**\n- \`${deleteGuild.count}\` guild entries\n- \`${deleteChannel.count}\` channel entries\n- \`${deleteMember.count}\` member entries\n- \`${deleteEmoji.count}\` emoji entries\n- \`${deleteSticker.count}\` sticker entries`);
   }
