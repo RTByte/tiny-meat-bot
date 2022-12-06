cbd7f17c758503f7608c994e9f6d99aa051edc2e
Rasmus Gerdin <rasmus@rasmusgerdin.com>
2022-12-06 20:58:12 +0100

    feat(listeners): initialize members in db on join

--- src/listeners/guilds/members/guildMemberAdd.ts
+++ src/listeners/guilds/members/guildMemberAdd.ts
@@ -1,10 +1,13 @@
 import { ApplyOptions } from "@sapphire/decorators";
 import { Events, Listener, ListenerOptions } from "@sapphire/framework";
+import { bold } from "colorette";
 import { GuildMember } from "discord.js";
 
 @ApplyOptions<ListenerOptions>({ event: Events.GuildMemberAdd })
 export class UserListener extends Listener {
 	public async run(member: GuildMember) {
+		await this.initializeMember(member);
+
 		await this.container.client.prisma.guild.update({
 			where: { id: String(member.guild.id) },
 			data: {
@@ -13,5 +16,21 @@ export class UserListener extends Listener {
 				}
 			}
 		});
+	}
+
+	private async initializeMember(member: GuildMember) {
+		const memberDb = await this.container.client.prisma.member.findFirst({ where: { id: member.id } });
+		if (!memberDb) {
+			this.container.logger.info(`Initializing entry for member ${bold(member.id)}...`);
+
+			await this.container.client.prisma.member.create({
+				data: {
+					id: member.id
+				}
+			}).catch(e => {
+				this.container.logger.error(`Failed to initialize member ${bold(member.id)}, error below.`);
+				this.container.logger.error(e);
+			});
+		}
 	}
 }
